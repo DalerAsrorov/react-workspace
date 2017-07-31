0683f828f1eb227b92fc584064e327b3b9a65a76
DalerAsrorov <asrorids@gmail.com>
2017-07-31 12:32:42 -0700

    added a basic page

--- practice/src/containers/Home.jsx
+++ practice/src/containers/Home.jsx
@@ -1,2 +1,11 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 
+class Home extends Component {
+    render() {
+        return (
+            <h3>Hello it's Home.</h3>
+        )
+    }
+}
+
+export default Home; 
